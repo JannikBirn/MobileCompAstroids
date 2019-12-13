d2762c5c4ca2652e6f2b7786e33da16025f5c7f2
Jannik Birn <jannik.birn@stud.hs-flensburg.de>
2019-12-13 21:21:41 +0100

    Added Astroid and Laser Classes

---
+++ AstroidsGame/app/src/main/java/com/example/astroids/game_src/entities/creatures/Astroid.java
@@ -0,0 +1,29 @@
+package com.example.astroids.game_src.entities.creatures;
+
+import android.graphics.Canvas;
+
+public class Astroid extends Creature {
+
+    private float health;
+    private float damage;
+    private int level;
+
+    public Astroid(float x, float y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Canvas c) {
+
+    }
+
+    @Override
+    public void onResume(String s) {
+
+    }
+}
