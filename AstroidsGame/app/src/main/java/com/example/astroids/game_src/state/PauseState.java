9063277d325cfe863c791d7dcc883485faeef248
Jannik Birn <jannik.birn@stud.hs-flensburg.de>
2019-12-14 17:38:03 +0100

    Added Persistent to gameState. GameState getting saved in onPause and loaded in onResume. Game Thread is stopped and restartet.

---
+++ AstroidsGame/app/src/main/java/com/example/astroids/game_src/state/PauseState.java
@@ -0,0 +1,24 @@
+package com.example.astroids.game_src.state;
+
+import android.graphics.Canvas;
+
+public class PauseState extends State {
+
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Canvas c) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+
+}
